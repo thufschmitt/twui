72b98c734854ac3ee8fc29a648fadc45f62a64b4
Casey Robinson <kc@rampantmonkey.com>
2014-05-06 08:41:12 -0400

    send task list when requested

--- twui.js
+++ twui.js
@@ -4,19 +4,21 @@ var taskFetcher = require('./lib/task_fetcher');
 
 var PORT = 2718
 
+var taskList
+
 var app = http.createServer( function (req, res) {
   res.writeHead(
     200,
-    {"content-type": "text/html"}
+    {"content-type": "application/json"}
   );
-  res.end('<p>Hello.</p>')
+  res.end(JSON.stringify(taskList))
 })
 
 when(taskFetcher.fetch(), function (tasks) {
-  console.log(tasks)
+  taskList = tasks
 }, function (err) {
   console.error(err)
 })
 
-//app.listen(PORT)
-//console.log('running at localhost:' + PORT + '...')
+app.listen(PORT)
+console.log('running at localhost:' + PORT + '...')
