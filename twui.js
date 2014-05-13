bd101b4518b659c071074aa57d4c5850240206c6
Casey Robinson <kc@rampantmonkey.com>
2014-05-12 20:04:04 -0400

    client api endpoint for creating tasks

--- twui.js
+++ twui.js
@@ -103,6 +103,38 @@ var app = http.createServer( function (req, res) {
     } else {
       badRequest(res)
     }
+  } else if (/^\/add/.test(req.url)) {
+    if(req.method === 'PUT') {
+      data = ''
+      req.on('data', function(chunk) { data += chunk.toString() })
+      req.on('end', function() {
+        try {
+          var taskdata = JSON.parse(data)
+          when(taskModifier.create(taskdata),
+            function (value) {
+              res.writeHead(201, {'content-type': 'applicaiton/json'})
+              res.end(JSON.stringify(value))
+            },
+            function (err) {
+              switch(err) {
+                case 'internal':
+                  res.writeHead(500)
+                  break
+                case 'malformed data':
+                  res.writeHead(400)
+                  break
+              }
+              res.writeHead({'content-type': 'text/plain'})
+              res.end()
+            }
+          )
+        } catch (e) {
+          badRequest(res)
+        }
+      })
+    } else {
+      badRequest(res)
+    }
   } else if ('/refresh' === req.url) {
     handleRefresh(res)
   } else {
