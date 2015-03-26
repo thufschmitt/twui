65ae8d53dd874af970a4733dc842f1b1cf0d833b
Casey Robinson <casey.robinson@distilnetworks.com>
2015-03-26 15:44:51 -0400

    serve static files

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+
+	log.Fatal(http.ListenAndServe(":2718", nil))
+}
