46a327472ed3e49dac72012282d81434814a12c9
Casey Robinson <casey@rampantmonkey.com>
2015-03-31 17:25:39 -0400

    switch to handlebars-style delimiters instead of erb

--- public/js/templater.js
+++ public/js/templater.js
@@ -10,11 +10,11 @@
           "with(obj){p.push('" +
           str
           .replace(/[\r\t\n]/g, " ")
-          .split("<%").join("\t")
-          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-          .replace(/\t=(.*?)%>/g, "',$1,'")
+          .split("{{").join("\t")
+          .replace(/((^|}})[^\t]*)'/g, "$1\r")
+          .replace(/\t(.*?)}}/g, "',$1,'")
           .split("\t").join("');")
-          .split("%>").join("p.push('")
+          .split("}}").join("p.push('")
           .split("\r").join("\\'")
           + "');}return p.join('');");
     return data ? fn( data ) : fn;
