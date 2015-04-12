2eaf73faf52bb2e630a975eed3dfcc299f82f954
Casey Robinson <casey@rampantmonkey.com>
2015-04-12 17:14:21 -0400

    plot all tasks with graphviz

---
+++ graph.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func init() {
+	cmdGraph.Run = runGraph
+}
+
+var cmdGraph = &Command {
+	UsageLine: "graph [filter]",
+	Short:     "graph",
+	Long: `
+graph produces a directed-acylic graph of tasks based on dependencies
+
+graph optionally takes a filter to limit the tasks displyed.
+The filter syntax is identical to taskwarrior's syntax.
+
+`,
+}
+
+const (
+	outputFilename = "task_graph.png"
+	penWidth = 1
+)
+
+func runGraph(c *Command, args []string) {
+	filter := strings.Join(args, " ")
+	tasks, err := FetchTasks(filter)
+	if err != nil {
+		log.Fatalf("Error fetching tasks\n")
+	}
+	digraph := generateDigraph(tasks)
+	dot := exec.Command("dot", "-T", "png")
+	stdin, err := dot.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stdout, err := dot.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = dot.Start(); err != nil {
+		log.Fatal(err)
+	}
+	go func(cmds []byte, in io.WriteCloser) {
+		defer in.Close()
+		for {
+			n, err := in.Write(cmds)
+			switch(err) {
+			case os.ErrInvalid:
+				log.Printf("could not write to dot pipe: %s\n", err)
+				return
+			case io.ErrShortWrite:
+				cmds = cmds[n:]
+			default:
+				return
+			}
+		}
+	}(digraph, stdin)
+
+	out, err := os.Create(outputFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := bufio.NewReader(stdout)
+	r.WriteTo(out)
+
+	err = dot.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+	out.Close()
+}
+
+func generateDigraph(tasks []Task) []byte {
+	dg := []byte("digraph dependencies { splines=true; overlap=ortho; rankdir=LR; weight=2;")
+
+	for _, task := range tasks {
+		style := "filled"
+		color := "white"
+		dg = append(dg, []byte(fmt.Sprintf("\"%s\"[shape=box][penwidth=%d][label=\"%d:%s\"][fillcolor=%s][style=%s]\n",
+		                       task.UUID,
+				       penWidth,
+				       task.Id,
+				       task.Description,
+				       color,
+				       style))...)
+	}
+
+	dg = append(dg, '}')
+	return dg
+}
